9ee5d2cb30ed9c8f406c6f10c0c0e3fb79a76d2c
Saerix <stasmorgun1@gmail.com>
2016-06-23 18:07:22 +0300

    123

--- src/com/devcolibri/Main.java
+++ src/com/devcolibri/Main.java
@@ -3,6 +3,6 @@ package com.devcolibri;
 public class Main {
 
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World!");
     }
 }
