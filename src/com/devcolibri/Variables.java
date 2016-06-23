8ed1e27e80c73f80efd534cd268cb2aabaaeec17
Saerix <stasmorgun1@gmail.com>
2016-06-23 17:17:16 +0300

    123

---
+++ src/com/devcolibri/Variables.java
@@ -0,0 +1,24 @@
+package com.devcolibri;
+
+public class Variables {
+    static byte b = 12;
+    static short c = 32000;
+    static int d = 99;
+    static long f = 333;
+    static float g = 3.14f;
+    static double v = 2.6;
+    static boolean bo = true;
+    static boolean bof = false;
+    public static void main (String[] args){
+
+        System.out.println("byte: " + b);
+        System.out.println("short:" + c);
+        System.out.println("int: " + d);
+        System.out.println("long: " + f);
+        System.out.println("float: " + g);
+        System.out.println("double: " + v);
+        System.out.println("boolean: " + bo);
+        System.out.println("boolean: " + bof);
+
+    }
+}
