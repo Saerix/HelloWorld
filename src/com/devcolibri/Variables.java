a481d59eebf5d1aa511c6cfb06938c959a5b2199
Saerix <stasmorgun1@gmail.com>
2016-06-23 18:02:47 +0300

    123

--- src/com/devcolibri/Variables.java
+++ src/com/devcolibri/Variables.java
@@ -6,19 +6,28 @@ public class Variables {
     static int d = 99;
     static long f = 333;
     static float g = 3.14f;
-    static double v = 2.5;
+    static double v = 2.9;
     static boolean bo = true;
     static boolean bof = false;
+    static final int CONST = 55;
+    static String st = "Hi Dev";
+    static char k ='d';
     public static void main (String[] args){
-
-        System.out.println("byte: " + b);
-        System.out.println("short:" + c);
+     int d = (int) v;
+        long f = k;
+        //Block is under construction
+     //   System.out.println("byte: " + b);
+      /*  System.out.println("short:" + c);
         System.out.println("int: " + d);
         System.out.println("long: " + f);
         System.out.println("float: " + g);
         System.out.println("double: " + v);
         System.out.println("boolean: " + bo);
         System.out.println("boolean: " + bof);
-
+        System.out.println(st);
+        System.out.println(k);
+        System.out.println(CONST);*/
+        System.out.println(d);
+        System.out.println(f);
     }
 }
