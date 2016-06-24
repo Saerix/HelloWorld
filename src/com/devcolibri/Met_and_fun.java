2d16258cd57973fc86fa109e949256bbc516bd10
Saerix <stasmorgun1@gmail.com>
2016-06-24 17:47:56 +0300

    123

---
+++ src/com/devcolibri/Met_and_fun.java
@@ -0,0 +1,27 @@
+package com.devcolibri;
+
+public class Met_and_fun {
+    public static void main (String[] args){
+        int val = calcSum(3,9); //Обращаемся к методу calcSum , передаем ему числа 3 и 9, и присваиваем переменной val то, что он нам вернул
+        System.out.println(val);
+        int rec = calcRec(1,0);
+        System.out.println(rec);
+    }
+    static int calcSum(int a, int b){ //Сам метод
+    int k = a + b;
+        //Метод отработал
+        return k;
+        //Метод вернул нам переменную sum
+    }static int calcRec(int a, int b){
+    int k = a + b;
+        if(k == 2)
+        {
+            k = calcRec (5,15);
+        }
+
+        return k;
+
+    }
+
+
+}
