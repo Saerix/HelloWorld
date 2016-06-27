1dfd07832339e6be49649acc56a87544d2d47d2f
Saerix <stasmorgun1@gmail.com>
2016-06-27 18:35:54 +0300

    Мучаем наследствование

---
+++ src/OOP/objects/Policeman.java
@@ -0,0 +1,20 @@
+package OOP.objects;
+
+public class Policeman extends People {   //extends People = наследуем класс People
+private String typeProfession;
+    public String getTypeProfession() {
+        return typeProfession;
+    }
+
+    public void setTypeProfession(String typeProfession) {
+        this.typeProfession = typeProfession;
+    }
+
+    public Policeman(String name, String lastName, int age, Cat cat, String typeProfession ) {  //Добавляем конструктор так как в классе People он тоже есть
+        super(name, lastName, age, cat);
+    }
+    @Override
+    public String toString() {
+        return "Персона: [Имя: " + getName() + " Фамилия: " + getLastName() + " Возраст: " + getAge() + " Профессия: " + getTypeProfession() +  " ]";
+    }
+}
--- src/OOP/objects/mainObj.java
+++ src/OOP/objects/mainObj.java
@@ -8,11 +8,14 @@ public class mainObj {
 public static void main(String[] args){
 
     cat = new Cat(2,"Персидский","Джон"); //Обращаемся к конструктору
-    people = new People("Stas", "Morhun", 3, cat);
+    people = new People("Stas", "Morhun", 33, cat);
     // people.setCat(cat);  Присваиваем объекту человек, объект кот
 
     System.out.println(people);
     people.getCat().say(); // обращаемся к объекту cat и вызываем его метод say
+    Policeman policeman = new Policeman ("Sergey", "Kobec" , 26 , cat, "Default");
+    policeman.setTypeProfession("Policeman");
+    System.out.println(policeman);
 
 }
 }
